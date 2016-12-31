539315a706f7ba24d69fe7ba8c5ea81e7cb90a6d
Oren <orengolan@gmail.com>
2016-12-31 15:49:11 -0800

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,13 @@
 const api = {
   login() {
     return {name: 'josh', email: 'josh@gmail.com'}
+  },
+  getDoctors() {
+    return [
+      {name: 'Wong'},
+      {name: 'Tan'},
+      {name: 'Ruth'}
+    ]
   }
 };
 
