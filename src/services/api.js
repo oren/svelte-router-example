cec23dd663f27e7a067c3e46abde3bc0cab351f0
Oren <orengolan@gmail.com>
2017-01-02 20:54:43 -0800

    success fetching data from an API

--- src/services/api.js
+++ src/services/api.js
@@ -3,19 +3,21 @@ const api = {
     return {name: 'josh', email: 'josh@gmail.com', token: 'abcde'}
   },
   getDoctors() {
-    fetch('https://api.github.com/users/mralexgray/repos', {
-      method: 'get'
-    }).then(function(response) {
-      console.log('success fetching', response);
-      return [
-        {name: 'Wong'},
-        {name: 'Tan'},
-        {name: 'Ruth'}
-      ];
-    }).catch(function(err) {
-      console.log('error fetching', err);
-      return [];
-    });
+    return new Promise(function(resolve, reject) {
+      fetch('https://api.github.com/users/mralexgray/repos', {
+        method: 'get'
+      }).then(function(response) {
+
+        response.json().then(function(data) {
+          console.log('success fetching', data);
+          resolve(data);
+        });
+
+      }).catch(function(err) {
+        console.log('error fetching', err);
+        reject(err);
+      });
+    })
 
   }
 };
