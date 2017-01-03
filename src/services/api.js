d29cdaac98a62fb316310a4a7cb8199d5c434875
Oren <orengolan@gmail.com>
2017-01-02 19:38:33 -0800

    server call

--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,20 @@ const api = {
     return {name: 'josh', email: 'josh@gmail.com', token: 'abcde'}
   },
   getDoctors() {
-    return [
-      {name: 'Wong'},
-      {name: 'Tan'},
-      {name: 'Ruth'}
-    ]
+    fetch('https://api.github.com/users/mralexgray/repos', {
+      method: 'get'
+    }).then(function(response) {
+      console.log('success fetching', response);
+      return [
+        {name: 'Wong'},
+        {name: 'Tan'},
+        {name: 'Ruth'}
+      ];
+    }).catch(function(err) {
+      console.log('error fetching', err);
+      return [];
+    });
+
   }
 };
 
